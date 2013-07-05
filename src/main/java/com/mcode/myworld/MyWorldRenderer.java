1542316229b1f8f8965bfe1b7896c2d1d9e5e159
Ming Liu <nongnongd@yahoo.com>
2013-07-04 21:02:03 -0400

    l

--- src/main/java/com/mcode/myworld/MyWorldRenderer.java
+++ src/main/java/com/mcode/myworld/MyWorldRenderer.java
@@ -35,6 +35,7 @@ import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
 import android.util.Log;
 
+import com.mcode.mjl.andriod.gles20.MVPMatrix;
 import com.mcode.mjl.andriod.gles20.WavefrontObj;
 
 
@@ -67,37 +68,18 @@ class MyWorldRenderer implements GLSurfaceView.Renderer {
         "  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); \n" +
         "}\n";
 
-    private float[] mMVPMatrix = new float[16];
-    private float[] mProjMatrix = new float[16];
-    private float[] mMMatrix = new float[16];
-    private float[] mVMatrix = new float[16];
+    MVPMatrix matrix = new MVPMatrix();
 
     private int mProgram;
     private int muMVPMatrixHandle;
     private int maPositionHandle;
 
     private Context mContext;
-    
+    private WavefrontObj wierdObj;
     
     public MyWorldRenderer(Context context) {
         mContext = context;
-        WavefrontObj wierdObj = new WavefrontObj(mContext.getResources().openRawResource(R.raw.creature), null);
-//        loadWavefront();
-//       
-//        vertexBuffer = ByteBuffer.allocateDirect(vertexList.size() * FLOAT_SIZE_BYTES).order(ByteOrder.nativeOrder()).asFloatBuffer();
-//        float[] vertexArray = new float[vertexList.size()];
-//        for(int i = 0; i < vertexList.size(); i ++) {
-//        	Log.i(TAG, "" + vertexList.get(i));
-//        	vertexArray[i] = vertexList.get(i);
-//        }
-//        vertexBuffer.put(vertexArray).position(0);
-//        indexBuffer = ByteBuffer.allocateDirect(indexList.size() * SHORT_SIZE_BYTES).order(ByteOrder.nativeOrder()).asShortBuffer();
-//        short[] indexArray = new short[indexList.size()];
-//        for(int i = 0; i < indexList.size(); i ++) {
-//        	indexArray[i] = indexList.get(i);
-//        }
-//        indexBuffer.put(indexArray).position(0);
-        
+        wierdObj = new WavefrontObj(matrix, mContext.getResources().openRawResource(R.raw.creature), null);        
     }
 
     public void onDrawFrame(GL10 glUnused) {
@@ -106,22 +88,25 @@ class MyWorldRenderer implements GLSurfaceView.Renderer {
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         GLES20.glClear( GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
         GLES20.glUseProgram(mProgram);
-
-        maPositionHandle = GLES20.glGetAttribLocation(mProgram, "aPosition");
-        GLES20.glEnableVertexAttribArray(maPositionHandle);
-        GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT, false, TRIANGLE_VERTICES_DATA_STRIDE_BYTES, vertexBuffer);
+//
+//        maPositionHandle = GLES20.glGetAttribLocation(mProgram, "aPosition");
+//        GLES20.glEnableVertexAttribArray(maPositionHandle);
+//        GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT, false, TRIANGLE_VERTICES_DATA_STRIDE_BYTES, vertexBuffer);
+//        
+//
+//        muMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+//
+//        Matrix.setRotateM(mMMatrix, 0, mAngleX, 0f, 1f, 0f);
+//        Matrix.rotateM(mMMatrix, 0, mAngleY, 1f, 0f, 0f);
+//        Matrix.multiplyMM(mMVPMatrix, 0, mVMatrix, 0, mMMatrix, 0);
+//        Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mMVPMatrix, 0);
+//
+//        GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix, 0);
+//
+//        GLES20.glDrawElements(GLES20.GL_TRIANGLES, indexList.size(), GLES20.GL_UNSIGNED_SHORT, indexBuffer); 
         
-
-        muMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
-
-        Matrix.setRotateM(mMMatrix, 0, mAngleX, 0f, 1f, 0f);
-        Matrix.rotateM(mMMatrix, 0, mAngleY, 1f, 0f, 0f);
-        Matrix.multiplyMM(mMVPMatrix, 0, mVMatrix, 0, mMMatrix, 0);
-        Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mMVPMatrix, 0);
-
-        GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix, 0);
-
-        GLES20.glDrawElements(GLES20.GL_TRIANGLES, indexList.size(), GLES20.GL_UNSIGNED_SHORT, indexBuffer); 
+        
+        wierdObj.drawFrame(mProgram,  "aPosition", "uMVPMatrix");
 
     }
 
@@ -130,35 +115,9 @@ class MyWorldRenderer implements GLSurfaceView.Renderer {
         // class's static methods instead.
         GLES20.glViewport(0, 0, width, height);
         float ratio = (float) width / height;
-        Matrix.frustumM(mProjMatrix, 0, -ratio, ratio, -1, 1, 1, 17);
+        Matrix.frustumM(matrix.getPMatrix(), 0, -ratio, ratio, -1, 1, 1, 17);
     }
     
-    private void loadWavefront() {
-    	InputStream is = mContext.getResources().openRawResource(R.raw.creature);
-    	BufferedReader br = new BufferedReader(new InputStreamReader(is));
-    	try {
-			String line = br.readLine();
-			while(line != null) {
-				String[] e = line.split(" ");
-				if(e.length > 1) {
-					if(e[0].equals("v")) {
-						for(int i = 1; i < e.length; i++) {
-							vertexList.add(Float.parseFloat(e[i]));
-						}
-					} else if (e[0].equals("f")) {
-						for(int i = 1; i < e.length; i++) {
-							indexList.add((short)(Short.parseShort(e[i]) - 1));
-							Log.w(TAG, e[i]);
-						}
-					}
-				}
-				line = br.readLine();
-			}
-		} catch (Exception e) {
-			Log.e(TAG, e.toString());
-		} 
-    }
-
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
         // Ignore the passed-in GL10 interface, and use the GLES20
         // class's static methods instead.
@@ -166,17 +125,8 @@ class MyWorldRenderer implements GLSurfaceView.Renderer {
         if (mProgram == 0) {
             return;
         }
-        maPositionHandle = GLES20.glGetAttribLocation(mProgram, "aPosition");
-        if (maPositionHandle == -1) {
-            throw new RuntimeException("Could not get attrib location for aPosition");
-        }
 
-        muMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
-        if (muMVPMatrixHandle == -1) {
-            throw new RuntimeException("Could not get attrib location for uMVPMatrix");
-        }
-
-        Matrix.setLookAtM(mVMatrix, 0, 0, 0, -5f, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        Matrix.setLookAtM(matrix.getVMatrix(), 0, 0, 0, -5f, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
     }
 
     private int loadShader(int shaderType, String source) {
