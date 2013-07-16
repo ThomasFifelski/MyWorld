b5f3bc5e2829224e3956b4e0a5435c26d7462df4
Ming Liu <nongnongd@yahoo.com>
2013-07-15 22:24:29 -0400

    some changes

--- src/main/java/com/mcode/myworld/MyWorldGLSurfaceView.java
+++ src/main/java/com/mcode/myworld/MyWorldGLSurfaceView.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2011 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.mcode.myworld;
 
 import android.content.Context;
@@ -21,20 +5,25 @@ import android.opengl.GLSurfaceView;
 import android.util.Log;
 import android.view.MotionEvent;
 
+import com.mcode.mjl.andriod.gles20.matrix.MVPMatrix;
+import com.mcode.mjl.andriod.gles20.matrix.MatrixTransformer;
+
 class MyWorldGLSurfaceView extends GLSurfaceView {
 	MyWorldRenderer renderer;
+	MVPMatrix matrix;
     public MyWorldGLSurfaceView(Context context) {
         super(context);
         setEGLContextClientVersion(2);
         renderer = new MyWorldRenderer(context);
+        matrix = renderer.getMatrix();
         setRenderer(renderer);
     }
     
     @Override
     public boolean onTouchEvent(MotionEvent e) {
     	Log.e("onTouchEvent", "X: " + e.getX());
-    	renderer.mAngleX = e.getX();
-    	renderer.mAngleY = e.getY();
+    	MatrixTransformer.rotate(matrix.getMMatrix(), e.getX(), 0, 0);
+    	//renderer.mAngleY = e.getY();
     	return true;
     }
 }
--- src/main/java/com/mcode/myworld/MyWorldRenderer.java
+++ src/main/java/com/mcode/myworld/MyWorldRenderer.java
@@ -1,5 +1,7 @@
 package com.mcode.myworld;
 
+import java.io.IOException;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -9,127 +11,48 @@ import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
 import android.util.Log;
 
-import com.mcode.mjl.andriod.gles20.MVPMatrix;
-import com.mcode.mjl.andriod.gles20.WavefrontObj;
+import com.mcode.mjl.andriod.gles20.matrix.MVPMatrix;
+import com.mcode.mjl.andriod.gles20.model.Model;
+import com.mcode.mjl.andriod.gles20.model.WavefrontObj;
+import com.mcode.mjl.andriod.gles20.shaders.SimpleShaderProgram;
 
 
 class MyWorldRenderer implements GLSurfaceView.Renderer {
 	public static String TAG = "GLES20TriangleRenderer"; // For logging purposes
-	
-	public volatile float mAngleX = 0.0f;
-	public volatile float mAngleY = 0.0f;
-
-    private final String mVertexShader =
-        "uniform mat4 uMVPMatrix;     \n" +
-        "attribute vec4 aPosition;    \n" +
-        "void main() {                \n" +
-        "  gl_Position = uMVPMatrix * aPosition; \n" +
-        "}\n";
-
-    private final String mFragmentShader =
-        "precision mediump float;\n" +
-        "void main() {\n" +
-        "  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); \n" +
-        "}\n";
 
     private MVPMatrix matrix = new MVPMatrix();
 
-    private int mProgram;
     private Context mContext;
-    private WavefrontObj wierdObj;
+    private Model wierdObj;
     
     public MyWorldRenderer(Context context) {
-        mContext = context;
-        wierdObj = new WavefrontObj(matrix, mContext.getResources().openRawResource(R.raw.creature), null);        
+        mContext = context;     
+    }
+    
+    public MVPMatrix getMatrix() {
+    	return matrix;
     }
 
     public void onDrawFrame(GL10 glUnused) {
-        // Ignore the passed-in GL10 interface, and use the GLES20
-        // class's static methods instead.
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         GLES20.glClear( GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
-        GLES20.glUseProgram(mProgram);
-
-//        maPositionHandle = GLES20.glGetAttribLocation(mProgram, "aPosition");
-//        GLES20.glEnableVertexAttribArray(maPositionHandle);
-//        GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT, false, TRIANGLE_VERTICES_DATA_STRIDE_BYTES, vertexBuffer);
-//        
-//
-//        muMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
-//
-//        Matrix.setRotateM(mMMatrix, 0, mAngleX, 0f, 1f, 0f);
-//        Matrix.rotateM(mMMatrix, 0, mAngleY, 1f, 0f, 0f);
-//        Matrix.multiplyMM(mMVPMatrix, 0, mVMatrix, 0, mMMatrix, 0);
-//        Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mMVPMatrix, 0);
-//
-//        GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix, 0);
-//
-//        GLES20.glDrawElements(GLES20.GL_TRIANGLES, indexList.size(), GLES20.GL_UNSIGNED_SHORT, indexBuffer); 
         
-        wierdObj.drawFrame(mProgram,  "aPosition", "uMVPMatrix");
+        wierdObj.drawFrame();
     }
 
     public void onSurfaceChanged(GL10 glUnused, int width, int height) {
-        // Ignore the passed-in GL10 interface, and use the GLES20
-        // class's static methods instead.
         GLES20.glViewport(0, 0, width, height);
         float ratio = (float) width / height;
         Matrix.frustumM(matrix.getPMatrix(), 0, -ratio, ratio, -1, 1, 1, 17);
     }
     
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
-        // Ignore the passed-in GL10 interface, and use the GLES20
-        // class's static methods instead.
-        mProgram = createProgram(mVertexShader, mFragmentShader);
-        if (mProgram == 0) {
-            return;
-        }
-
+    	try {
+    		wierdObj = new WavefrontObj(new SimpleShaderProgram(), matrix, mContext.getAssets().open("cube.obj"), mContext);  
+    	} catch(IOException e) {
+    		// file not found
+    		Log.e(TAG, "Model file not found!");
+    	}
         Matrix.setLookAtM(matrix.getVMatrix(), 0, 0, 0, -5f, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
-    }
-
-    private int loadShader(int shaderType, String source) {
-        int shader = GLES20.glCreateShader(shaderType);
-        if (shader != 0) {
-            GLES20.glShaderSource(shader, source);
-            GLES20.glCompileShader(shader);
-            int[] compiled = new int[1];
-            GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
-            if (compiled[0] == 0) {
-                Log.e(TAG, "Could not compile shader " + shaderType + ":");
-                Log.e(TAG, GLES20.glGetShaderInfoLog(shader));
-                GLES20.glDeleteShader(shader);
-                shader = 0;
-            }
-        }
-        return shader;
-    }
-
-    private int createProgram(String vertexSource, String fragmentSource) {
-        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource);
-        if (vertexShader == 0) {
-            return 0;
-        }
-
-        int pixelShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource);
-        if (pixelShader == 0) {
-            return 0;
-        }
-
-        int program = GLES20.glCreateProgram();
-        if (program != 0) {
-            GLES20.glAttachShader(program, vertexShader);
-            GLES20.glAttachShader(program, pixelShader);
-            GLES20.glLinkProgram(program);
-            int[] linkStatus = new int[1];
-            GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0);
-            if (linkStatus[0] != GLES20.GL_TRUE) {
-                Log.e(TAG, "Could not link program: ");
-                Log.e(TAG, GLES20.glGetProgramInfoLog(program));
-                GLES20.glDeleteProgram(program);
-                program = 0;
-            }
-        }
-        return program;
     }
 }
